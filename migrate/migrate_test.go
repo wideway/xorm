bde63467a627a61ea67481263ff83cfb9c44163e
segeln <willis_z@126.com>
2021-01-29 12:00:34 +0800

    edit

--- migrate/migrate_test.go
+++ migrate/migrate_test.go
@@ -6,9 +6,9 @@ import (
 	"os"
 	"testing"
 
-	"github.com/go-xorm/xorm"
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/stretchr/testify/assert"
+	"github.com/wideway/xorm"
 )
 
 type Person struct {
